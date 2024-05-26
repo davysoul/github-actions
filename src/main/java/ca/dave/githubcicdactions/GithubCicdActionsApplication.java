8aabe2111c11cefdb005971942c64596b6b332d0
davysoul <d_av_soul@hotmail.com>
2024-05-26 19:35:49 -0400

    Fixe endpoint of welcome method

--- src/main/java/ca/dave/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/ca/dave/githubcicdactions/GithubCicdActionsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class GithubCicdActionsApplication {
-    @GetMapping("/wecome")
+    @GetMapping("/welcome")
     public String welcome(){
         return "Welcome the programmers";
     }
