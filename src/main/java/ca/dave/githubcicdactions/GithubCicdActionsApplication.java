ec6202b42278a50cc6b01162031d4c599c57c5a3
davysoul <d_av_soul@hotmail.com>
2024-05-26 12:58:55 -0400

    first commit for code

---
+++ src/main/java/ca/dave/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,20 @@
+package ca.dave.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+    @GetMapping("/wecome")
+    public String welcome(){
+        return "Welcome the programmers";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
